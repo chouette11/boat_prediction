776bc118e03ed9fbcae87ad3bd6cc0594e11736b
chouette11 <keifukuda1115@gmail.com>
2025-08-08 02:12:23 +0900

    Add session delete

---
+++ sql/session_delete.py
@@ -0,0 +1,25 @@
+def check(conn):
+    cur = conn.cursor()
+    # 数を確認
+    ex = """
+SELECT pg_terminate_backend(28716);
+"""
+    print(ex)
+    cur.execute(ex)
+    print(cur.fetchall())
+
+    cur.close(); conn.close()
+
+if __name__ == "__main__":
+    import os
+    from dotenv import load_dotenv
+    import psycopg2
+    load_dotenv(override=True)
+    conn = psycopg2.connect(
+        dbname=os.getenv("PGDATABASE", "ver2_2"),
+        user=os.getenv("PGUSER", "keiichiro"),
+        host=os.getenv("PGHOST", "localhost"),
+        port=os.getenv("PGPORT", "5432")
+    )
+    # confirm(conn=conn)
+    check(conn=conn)
